b7ad1db29369a888481512af82318538e04f1846
Frank Mueller <mue@tideland.biz>
2016-01-29 10:39:48 +0100

    Adopted vim-airline changes.

--- setup-vim.sh
+++ setup-vim.sh
@@ -14,7 +14,8 @@ git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/bling/vim-airline
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/vim-erlang/vim-dialyzer.git
 git clone https://github.com/tpope/vim-dispatch.git
 git clone https://github.com/vim-erlang/vim-erlang-omnicomplete.git
